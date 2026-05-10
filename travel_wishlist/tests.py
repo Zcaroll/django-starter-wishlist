33f5794f4bf7907f2d4029df6cc77d86f635670b
Zcaroll <qc0193ki@go.minneapolis.edu>
2026-05-10 05:33:10 -0500

    begun work on tests

--- travel_wishlist/tests.py
+++ travel_wishlist/tests.py
@@ -1,3 +1,7 @@
-from django.test import TestCase
-
+from django.test import TestCase # extention of TestCase that is specific to testing Django applications
+from django.urls import reverse # used to get the URL for a view by its name
 # Create your tests here.
+
+from.models import Place
+
+# class TestHomePage(TestCase):
