b1ca547a1152eff0ddc108f610c4653b86fe49d4
Zcaroll <qc0193ki@go.minneapolis.edu>
2026-05-10 16:05:28 -0500

    tried troubleshooting. added place detail html

--- travel_wishlist/models.py
+++ travel_wishlist/models.py
@@ -15,6 +15,6 @@ class Place(models.Model):
     def __str__(self):
         # not displayed to user, but might be helpful for the dev
         photo_str = self.photo.url if self.photo else 'No photo'
-        notes_str = self.notes[100: ] # 100 characters of notes
+        notes_str = self.notes[100: ] if self.notes else 'No notes' # only show the first 100 characters of the notes
         return f'{self.name} (Visited: {self.visited}) on {self.date_visited}. Notes: {notes_str} - Photo: {photo_str}'
     
--- travel_wishlist/views.py
+++ travel_wishlist/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Place
 from .forms import NewPlaceForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -10,17 +11,18 @@ def place_list(request):
     if request.method == 'POST': 
         #create new place
         form = NewPlaceForm(request.POST) 
-        place = form.save() #makes a model object from form
+        place = form.save(commit=False) #makes a model object from form
+        place.user = request.user # associate the place with the current user
         if form.is_valid(): # validation against database constraints
             place.save() # save the new place to the database
             return redirect('place_list') # reloads home page
         
 
-    places = Place.objects.all() # get all the objects from the database
+    places = Place.objects.filter(user=request.user).filter(visited=False).order_by('name') # get all places from the database that are not visited; filter by user
     new_place_form = NewPlaceForm() # used to create HTML
     return render(request, 'travel_wishlist/wishlist.html', {'places': places, 'new_place_form': new_place_form})
 
-
+@login_required
 def places_visited(request):
     visited = Place.objects.filter(visited=True) 
     return render(request, 'travel_wishlist/visited.html', {'visited': visited})
@@ -40,3 +42,8 @@ def about(request):
     author = 'Zach'
     about = 'A website of a wishlist of places I want to visit.'
     return render(request, 'travel_wishlist/about.html', {'author': author, 'about': about})
+
+@login_required
+def place_details(request, place_pk):
+    place = get_object_or_404(Place, pk=place_pk) # get the place from the database; if it doesn't exist, return 404 error
+    return render(request, 'travel_wishlist/place_details.html', {'place': place})
