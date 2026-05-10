c768374e012dbf0433a0d59112e7d98680d29804
Zcaroll <qc0193ki@go.minneapolis.edu>
2026-05-10 17:20:15 -0500

    added function for user to delete

--- travel_wishlist/views.py
+++ travel_wishlist/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Place
 from .forms import NewPlaceForm
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
 
 # Create your views here.
 
@@ -27,12 +28,18 @@ def places_visited(request):
     visited = Place.objects.filter(visited=True) 
     return render(request, 'travel_wishlist/visited.html', {'visited': visited})
 
+@login_required
 def place_was_visited(request, place_pk): #pk is primary key
     if request.method == 'POST':
         # place = Place.objects.get(pk=place_pk) # get the place from the database
         place = get_object_or_404(Place, pk=place_pk) # get the place from the database; if it doesn't exist, return 404 error
-        place.visited = True # change visited to true
-        place.save() # save the change to the database
+        # prevents malicious user from marking a place as visited if it does not belong to them
+        if place.user == request.user: # if the place does not belong to the current user, return 403 error
+            place.visited = True # change visited to true
+            place.save() # save the change to the database
+        else:
+            return HttpResponseForbidden() # return 403 error if the place does not belong to the current user
+
 
     # return ridirect('places_visited') # directs to places visited page
     return redirect('place_list') # name of path; redirect to wishlist
@@ -47,3 +54,13 @@ def about(request):
 def place_details(request, place_pk):
     place = get_object_or_404(Place, pk=place_pk) # get the place from the database; if it doesn't exist, return 404 error
     return render(request, 'travel_wishlist/place_details.html', {'place': place})
+
+@login_required
+# allows user to delete a place from the database
+def delete_place(request, place_pk):
+    place = get_object_or_404(Place, pk=place_pk) 
+    if place.user == request.user: 
+          place.delete() 
+          return redirect('place_list') 
+    else:
+          return HttpResponseForbidden() 
